6ab181ed617da29261d539e84c21fbcdb5f26ce3
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-04-01 16:58:56 -0400

    working local dev

---
+++ README.md
@@ -0,0 +1,7 @@
+# zeit-now-example
+> Small example using Zeit Now v2
+
+More info:
+
+- https://glebbahmutov.com/blog/zeit-now-v2-workflow/
+- https://zeit.co/blog/now-2
--- hooks/foo/index.ts
+++ hooks/foo/index.ts
@@ -1,7 +1,9 @@
-const { send } = require('micro')
+import { IncomingMessage, ServerResponse } from 'http'
+import {send} from 'micro'
+
 module.exports = async (req: IncomingMessage, res: ServerResponse) => {
   try {
-    send(res, 200)
+    send(res, 200, 'foo')
   } catch (e) {
     console.error(e.message)
     return send(res, 400)
